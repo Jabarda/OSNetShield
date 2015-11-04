f2c9b947f154d512713982c984a0409d8c5260b5
Jabarda <stalkercxz@yandex.ru>
2015-11-04 13:45:59 +0300

    Added Java Version

---
+++ EasyFirewall/src/EasyFirewall.java
@@ -0,0 +1,72 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
+/**
+ * Created by 1 on 03.11.2015.
+ */
+public class EasyFirewall {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("EasyFirewall");
+
+        window.setLayout(new GridLayout());
+        window.setSize(500, 500);
+        JButton close = new JButton(); //закрывает
+        close.setText("Close Ethernet");
+        JButton open = new JButton();  //открывает
+        open.setText("Open Ethernet");
+        close.addMouseListener(new MouseListener() {
+
+            public void mouseClicked(MouseEvent event) {
+                try {
+                    Runtime.getRuntime().exec("netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                };
+            }
+
+            public void mouseEntered(MouseEvent event) {
+            }
+
+            public void mouseExited(MouseEvent event) {
+            }
+
+            public void mousePressed(MouseEvent event) {
+            }
+
+            public void mouseReleased(MouseEvent event) {
+            }
+
+        });
+        open.addMouseListener(new MouseListener() {
+
+            public void mouseClicked(MouseEvent event) {
+                try {
+                    Runtime.getRuntime().exec("netsh advfirewall set allprofiles firewallpolicy blockinbound,allowoutbound");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                };
+            }
+
+            public void mouseEntered(MouseEvent event) {
+            }
+
+            public void mouseExited(MouseEvent event) {
+            }
+
+            public void mousePressed(MouseEvent event) {
+            }
+
+            public void mouseReleased(MouseEvent event) {
+            }
+
+        });
+
+        window.add(open);
+        window.add(close);
+        window.setVisible(true);
+    }
+}
