4a8206c0d2eafffb1d257ac0d34ea0ef6bed2c64
carnwyr <carnwyr@gmail.com>
2015-12-09 18:10:16 +0300

    Corrections, comments and stuff

--- OSNetShield/OSNetShield/Basic_window.h
+++ OSNetShield/OSNetShield/Basic_window.h
@@ -2,10 +2,15 @@
 #include "resource.h"
 #include "cFwAccess.h"
 
-// dialog window Basic_window
-
+///
+/// Method which puts the main window into its own thread
+///
 void thread_Proc(cFwAccess *pFwAccessIn);
 
+///
+/// The main window which allows block and unblock IPs
+/// Also provides access to other windows
+///
 class Basic_window : public CDialog
 {
 	DECLARE_DYNAMIC(Basic_window)
@@ -26,13 +31,11 @@ public:
 	
 	afx_msg void OnBnClickedBlock();
 	afx_msg void OnBnClickedUnblock();
-	afx_msg void OnEnChangeEdit1();
-	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnBnClickedTCP();
+	afx_msg void OnBnClickedBase();
+	afx_msg void OnBnClickedExit();
 
 private:
 	cFwAccess *pFwAccess;
-public:
-	afx_msg void OnBnClickedButton1();
-	afx_msg void OnBnClickedButton2();
-	afx_msg void OnBnClickedButton3();
+	
 };
